103189d8dabf6c7ea7cf4220422f29ac23481adf
t7reyeslua <t7.reyeslua@gmail.com>
2015-04-30 03:52:48 +0200

    Added table showing updated RSSI values of each detected beacon
    *Includes button to star/stop scanning

--- mobile/src/main/java/tudelft/beaconscanner/MainActivity.java
+++ mobile/src/main/java/tudelft/beaconscanner/MainActivity.java
@@ -4,19 +4,29 @@ import com.gimbal.android.BeaconEventListener;
 import com.gimbal.android.BeaconManager;
 import com.gimbal.android.BeaconSighting;
 import com.gimbal.android.Gimbal;
+import com.github.clans.fab.FloatingActionButton;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ExpandableListView;
 
+import java.util.ArrayList;
+
+import tudelft.beaconscanner.ui_helpers.ExpandableListAdapterBeacons;
 
 
 public class MainActivity extends ActionBarActivity {
     private BeaconEventListener beaconSightingListener;
     private BeaconManager beaconManager;
-
+    private ExpandableListView mExpandableListBeacons;
+    private ArrayList<BeaconSighting> groupItem = new ArrayList<>();
+    private ArrayList<Object> childItem = new ArrayList<>();
+    private FloatingActionButton fab;
+    private Boolean scanning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +35,97 @@ public class MainActivity extends ActionBarActivity {
 
         Gimbal.setApiKey(this.getApplication(), "0ef2df58-9665-49c9-b914-015e057751aa");
 
+        mExpandableListBeacons = (ExpandableListView) findViewById(R.id.exp_list);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        configureFab();
+
         beaconSightingListener = new BeaconEventListener() {
             @Override
             public void onBeaconSighting(BeaconSighting sighting) {
                 Log.i("INFO", sighting.toString());
+                updateScanResults(sighting);
             }
         };
+
         beaconManager = new BeaconManager();
-        beaconManager.addListener(beaconSightingListener);
 
-        beaconManager.startListening();
+    }
+
+    private void configureFab(){
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("FAB", "Click " + scanning);
+                if (!scanning){
+                    beaconManager.addListener(beaconSightingListener);
+                    beaconManager.startListening();
+                } else {
+                    beaconManager.removeListener(beaconSightingListener);
+                    beaconManager.stopListening();
+                }
+                fab.setImageResource(
+                        scanning ? R.drawable.ic_action_play : R.drawable.ic_action_stop);
+                scanning = !scanning;
+            }
+        });
+
+    }
 
 
+    private void updateScanResults(BeaconSighting sighting){
+        String beaconId = sighting.getBeacon().getIdentifier();
+        childItem.clear();
+        Boolean found  = false;
+        ArrayList<BeaconSighting> tempList = new ArrayList<>();
+        for (BeaconSighting bs : groupItem){
+            ArrayList<BeaconSighting> child = new ArrayList<>();
+            if (bs.getBeacon().getIdentifier().equals(beaconId)){
+                tempList.add(sighting);
+                child.add(sighting);
+                found = true;
+            } else {
+                tempList.add(bs);
+                child.add(bs);
+            }
+            childItem.add(child);
+        }
+        if (!found){
+            tempList.add(sighting);
+            ArrayList<BeaconSighting> child = new ArrayList<>();
+            child.add(sighting);
+            childItem.add(child);
+        }
+        groupItem = new ArrayList<>(tempList);
+        ArrayList<Boolean>expanded = new ArrayList<>();
+        for ( int i = 0; i < groupItem.size(); i++ ) {
+            try {
+                expanded.add(mExpandableListBeacons.isGroupExpanded(i));
+            } catch (Exception e) {
+                expanded.add(false);
+            }
+        }
+        mExpandableListBeacons.setAdapter(new ExpandableListAdapterBeacons(this, groupItem, childItem));
+        mExpandableListBeacons.setOnGroupClickListener(new ExpDrawerGroupClickListener());
+        for ( int i = 0; i < groupItem.size(); i++ ) {
+            if (expanded.get(i)) {
+                mExpandableListBeacons.expandGroup(i);
+            }
+        }
+    }
+
+    private class ExpDrawerGroupClickListener implements ExpandableListView.OnGroupClickListener {
+        @Override
+        public boolean onGroupClick(ExpandableListView parent, View v,
+                int groupPosition, long id) {
+
+            if (parent.isGroupExpanded(groupPosition)){
+                parent.collapseGroup(groupPosition);
+            }else {
+                parent.expandGroup(groupPosition, true);
+            }
+            return true;
+        }
     }
 
     @Override
---
+++ mobile/src/main/java/tudelft/beaconscanner/ui_helpers/ExpandableListAdapterBeacons.java
@@ -0,0 +1,179 @@
+package tudelft.beaconscanner.ui_helpers;
+
+import com.gimbal.android.Beacon;
+import com.gimbal.android.BeaconSighting;
+
+import tudelft.beaconscanner.R;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ExpandableListAdapterBeacons extends BaseExpandableListAdapter {
+
+    public ArrayList<BeaconSighting> tempChild;
+    public ArrayList<BeaconSighting> groupItem = new ArrayList<BeaconSighting>();
+    public ArrayList<Object> Childtem = new ArrayList<Object>();
+    public LayoutInflater minflater;
+    public Activity activity;
+    private final Context context;
+
+    private static final int[] EMPTY_STATE_SET = {};
+    private static final int[] GROUP_EXPANDED_STATE_SET =
+            {android.R.attr.state_expanded};
+    private static final int[][] GROUP_STATE_SETS = {
+            EMPTY_STATE_SET, // 0
+            GROUP_EXPANDED_STATE_SET // 1
+    };
+
+    public ExpandableListAdapterBeacons(Context context, ArrayList<BeaconSighting> grList,
+            ArrayList<Object> childItem) {
+        this.context = context;
+        groupItem = grList;
+        this.Childtem = childItem;
+    }
+
+    public void setInflater(LayoutInflater mInflater, Activity act) {
+        this.minflater = mInflater;
+        activity = act;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return null;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, final int childPosition,
+            boolean isLastChild, View convertView, ViewGroup parent) {
+        tempChild = (ArrayList<BeaconSighting>) Childtem.get(groupPosition);
+        TextView text = null;
+        if (convertView == null)
+        {
+            LayoutInflater layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.list_subitem_scanned_beacon,parent,false);
+        }
+
+        TextView twID = (TextView) convertView.findViewById(R.id.beaconId);
+        TextView twBatt = (TextView) convertView.findViewById(R.id.beaconBattery);
+        TextView twTemp = (TextView) convertView.findViewById(R.id.beaconTemperature);
+        TextView twDate = (TextView) convertView.findViewById(R.id.beaconDate);
+
+        Beacon beacon = tempChild.get(childPosition).getBeacon();
+        Date date = new Date(tempChild.get(childPosition).getTimeInMillis());
+
+        twID.setText(beacon.getIdentifier());
+        twBatt.setText(String.valueOf(beacon.getBatteryLevel()));
+        twTemp.setText(String.valueOf((beacon.getTemperature() - 32)*5/9) + "°C");
+        SimpleDateFormat dt = new SimpleDateFormat("hh:mm:ss");
+        twDate.setText(dt.format(date));
+
+        convertView.setTag(tempChild.get(childPosition));
+        return convertView;
+    }
+
+
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return ((ArrayList<BeaconSighting>) Childtem.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return null;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return groupItem.size();
+    }
+
+    @Override
+    public void onGroupCollapsed(int groupPosition) {
+        super.onGroupCollapsed(groupPosition);
+    }
+
+    @Override
+    public void onGroupExpanded(int groupPosition) {
+        super.onGroupExpanded(groupPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent)
+    {
+        if (convertView == null)
+        {
+            LayoutInflater layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.list_item_scanned_beacon,parent,false);
+        }
+
+
+        String name = groupItem.get(groupPosition).getBeacon().getName();
+        Integer rssi = groupItem.get(groupPosition).getRSSI();
+
+        TextView twName = (TextView) convertView.findViewById(R.id.beaconName);
+        TextView twRSSI = (TextView) convertView.findViewById(R.id.beaconRSSI);
+
+        twName.setText(name);
+        twRSSI.setText(String.valueOf(rssi) + " dB");
+        convertView.setTag(groupItem.get(groupPosition));
+
+        View ind = convertView.findViewById(R.id.explist_indicator);
+        if (ind != null) {
+            ImageView indicator = (ImageView) ind;
+            if (getChildrenCount(groupPosition) == 0) {
+
+                indicator.setVisibility(View.INVISIBLE);
+            } else {
+                indicator.setVisibility(View.INVISIBLE);
+                indicator.setImageResource(
+                        isExpanded ? R.drawable.ic_action_collapse : R.drawable.ic_action_expand);
+            }
+        }
+
+        TextView tw = (TextView) convertView.findViewById(R.id.explist_bar);
+
+
+        if (rssi < -80) {
+            tw.setBackgroundColor(context.getResources().getColor(R.color.Crimson));
+        } else if (rssi < -60) {
+            tw.setBackgroundColor(context.getResources().getColor(R.color.OrangeRed));
+        } else if (rssi < -40) {
+            tw.setBackgroundColor(context.getResources().getColor(R.color.YellowGreen));
+        }else {
+            tw.setBackgroundColor(context.getResources().getColor(R.color.Green));
+        }
+
+        return convertView;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
+}
